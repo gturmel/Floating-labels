11b5ac785b1917e3cbbb90535c576ae45f9af16c
Greg Turmel <gregturmel@gmail.com>
2016-04-29 13:34:24 -0400

    vanilla javascript for inputs and labels is too long

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,36 @@
 //add event listener to the input (focus/blur)
-var input = document.getElementById("first-name");
-var label = document.getElementById("label1");
+var firstNameInput = document.getElementById("first-name-input");
+var lastNameInput = document.getElementById('last-name-input');
+var firstLabel = document.getElementById("first-name");
+var lastLabel = document.getElementById("last-name");
 
 
-input.addEventListener("focus", function(){
-   console.log('is too a function')
-   label.classList.add("label");
-   label.classList.remove('focus');
+var inputClass = document.getElementsByClassName('space');
+console.log(inputClass);
+//add and remove guts for event listener functions
+
+
+//focus/blur event listeners
+firstNameInput.addEventListener("focus", function(){
+   firstLabel.classList.add("label-up");
+   firstLabel.classList.remove('label-down');
 });
 
-input.addEventListener("blur", function(){
-   if(input.value === ""){
-      label.classList.remove("label");
-      label.classList.add('focus');
+firstNameInput.addEventListener("blur", function(){
+   if(firstNameInput.value === ""){
+      firstLabel.classList.remove("label-up");
+      firstLabel.classList.add('label-down');
+   }
+});
+
+lastNameInput.addEventListener("focus", function(){
+   lastLabel.classList.add("label-up");
+   lastLabel.classList.remove('label-down');
+});
+
+lastNameInput.addEventListener("blur", function(){
+   if(lastNameInput.value === ""){
+      lastLabel.classList.remove("label-up");
+      lastLabel.classList.add('label-down');
    }
 });
